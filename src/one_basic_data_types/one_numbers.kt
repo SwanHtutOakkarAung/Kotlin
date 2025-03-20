b43f23ffce38a4fdc05c707427c8d45484847ecb
SwanHtutOakkarAung <swannhtutokag@gmail.com>
2025-03-20 16:16:30 +0700

    my-first-commit

---
+++ src/one_basic_data_types/one_numbers.kt
@@ -0,0 +1,21 @@
+package one_basic_data_types
+//var -> read and write
+//val -> read
+//identifier variable-name : data-type = value
+fun main(){
+    //Unsigned Integers
+    //Byte 8 bits 2^7
+    //Short 16 bits 2^15
+    //Int 32 bits
+    var age: Int = 26
+    age = 18
+    //Long 64 bits
+
+    //Signed Integers
+
+    //Floating Points
+    //Float 32 bits
+    var weight : Float = 34.5F
+    //Double 64 bits
+    var height : Double = 3.4
+}
---
+++ src/one_basic_data_types/string.kt
@@ -0,0 +1,7 @@
+package one_basic_data_types
+
+fun main(){
+    val name : String = "Penye"
+    val age : Byte = 26
+    println("My name is $name and I am $age years old.")
+}
